8921852ffb1eea5d9b6dca690a5133e5e6c21dc3
rishikeshwagh <54074706+rishikeshwagh@users.noreply.github.com>
2020-01-23 15:39:16 +0530

    Add files via upload

---
+++ AirlineTicketManager/src/main/java/com/airline/demo/AirlineTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package com.airline.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineTicketBookingApplication.class, args);
+	}
+
+}
